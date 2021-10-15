2447903551f4f6b72a945826bfc9c3bee1bfc861
Danny Gibbons <51105241+degibbons@users.noreply.github.com>
2021-10-15 13:23:02 -0400

    Add setup.py pre-finish

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Tilibot',
+      version='1.0', # Fix
+      description='Tiliqua Biomechanics Emulation', 
+      author='Dan Gibbons',
+      author_email='danegibbons@gmail.com',
+      url='', # Fix
+      packages=['distutils', 'distutils.command'], # Fix
+     )
